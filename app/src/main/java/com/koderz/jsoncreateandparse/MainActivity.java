03c03df4068486c74f30f76c5a28cd262fd64ae9
kushalausula <kushalkanna@gmail.com>
2016-12-15 08:50:06 +0530

    update

--- app/src/main/java/com/koderz/jsoncreateandparse/MainActivity.java
+++ app/src/main/java/com/koderz/jsoncreateandparse/MainActivity.java
@@ -21,12 +21,48 @@ public class MainActivity extends AppCompatActivity {
         json = getString(R.string.jsonString);
         parseJsonAndPrint(json);
 
+
+        try {
+            createJSON();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createJSON() throws JSONException {
+        JSONObject jObj=new JSONObject();
+
+        jObj.put("name","kushal");
+        jObj.put("id","1");
+
+
+        JSONObject jSubObj=new JSONObject();
+        jSubObj.put("mobile","7893335553");
+        jSubObj.put("age","28");
+
+        /*adding sub obj to main json obj*/
+
+        jObj.put("more_details",jSubObj);
+
+        /*********************************/
+
+        /*Form json array*/
+        JSONArray jArray=new JSONArray();
+
+        for(int i=0;i<10;i++) {
+            JSONObject jsonArr_Obj = new JSONObject();
+            jsonArr_Obj.put("sub"+i,i+" s");
+            jsonArr_Obj.put("marks"+i,i+" m");
+            jArray.put(jsonArr_Obj);
+        }
+        jObj.put("Marks_Details",jArray);
+        Log.i("TAG", "createJSON: "+jObj.toString());
     }
 
     private void parseJsonAndPrint(String json) {
 
         try {
-            JSONObject jsonOjMain = new JSONObject(json);
+                       JSONObject jsonOjMain = new JSONObject(json);
 
             String greet = jsonOjMain.getString("greet");
             boolean inClass = jsonOjMain.getBoolean("inclass");
